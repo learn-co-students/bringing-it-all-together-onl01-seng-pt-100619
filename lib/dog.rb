09576846a3a5932e9da282e2f060c45cb414b038
allison cortez <allizetorc@gmail.com>
2019-12-23 02:40:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,4 +43,31 @@ class Dog
     dog
   end
   
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id,name: name,breed: breed)
+  end 
+  
+  def self.find_by_id(id)
+    sql = <<-SQL 
+      SELECT * FROM dogs 
+      WHERE id = ? 
+      SQL
+      
+      DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def self.find_or_create_by
+  end
+  
+  def self.find_by_name 
+  end 
+  
+  def update 
+  end 
+  
 end
